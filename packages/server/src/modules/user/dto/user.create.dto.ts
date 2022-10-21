09b98f2e40d6ad36dd236ddbb28e9e852a596033
Sergei Zheludkov <58815292+SergoZheludkov@users.noreply.github.com>
2022-10-21 16:47:46 +0300

    CB-0004 | server: swagger init and setup

--- packages/server/src/modules/user/dto/user.create.dto.ts
+++ packages/server/src/modules/user/dto/user.create.dto.ts
@@ -4,29 +4,54 @@ import {
   IsNotEmpty,
   IsEnum,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { LangEnum } from '../../../types/lang.enum';
 
 class UserCreateDto {
+  @ApiProperty({
+    required: true,
+    example: '258000010',
+  })
   @IsNotEmpty()
   @IsString()
   id: string;
 
+  @ApiProperty({
+    required: false,
+    example: 'Sergei',
+  })
   @IsOptional()
   @IsString()
   firstname?: string;
 
+  @ApiProperty({
+    required: false,
+    example: 'Zheludkov',
+  })
   @IsOptional()
   @IsString()
   lastname?: string;
 
+  @ApiProperty({
+    required: false,
+    example: 'sergozheludkov',
+  })
   @IsOptional()
   @IsString()
   username?: string;
 
+  @ApiProperty({
+    required: false,
+    example: '258000010',
+  })
   @IsOptional()
   @IsString()
   who_invited?: string;
 
+  @ApiProperty({
+    required: true,
+    enum: LangEnum,
+  })
   @IsNotEmpty()
   @IsEnum(LangEnum)
   lang: string;
