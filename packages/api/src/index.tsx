584f40cfa125b4df8a3933099bf852cf48ab99a9
Sergei Zheludkov <58815292+SergoZheludkov@users.noreply.github.com>
2022-10-23 12:57:37 +0300

    CB-0011 | api: use-query hook for fetch

---
+++ packages/api/src/index.tsx
@@ -0,0 +1,2 @@
+export { ApiProvider, useApi } from './provider';
+export { useQuery } from './use-query';
---
+++ packages/api/src/use-query.tsx
@@ -0,0 +1,106 @@
+import {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
+import { ApiError, CancelablePromise } from './generated';
+
+export type QueryOptions = {
+  isLazy?: boolean;
+}
+
+type QueryState<T, E = ApiError> = {
+  isLoading: boolean;
+  isError: boolean;
+  isSuccess: boolean;
+  error?: E;
+  data?: T;
+  statusCode?: number;
+}
+
+export type Query<T, E = ApiError, A = any> = QueryState<T, E> & {
+  fetch: (...arg: A[]) => Promise<QueryState<T, E>>
+}
+
+function useQuery<T, A, E extends ApiError = ApiError>(
+  id: string,
+  callback: (...arg: A[]) => CancelablePromise<T>,
+  { isLazy = false }: QueryOptions = {},
+): Query<T, E, A> {
+  const [query, setQuery] = useState<QueryState<T, E>>({
+    isLoading: !isLazy,
+    isError: false,
+    isSuccess: false,
+  });
+
+  const fetch = useCallback(
+    async (...arg: A[]): Promise<QueryState<T, E>> => {
+      setQuery({
+        isLoading: true,
+        isError: false,
+        isSuccess: false,
+      });
+
+      try {
+        const data = await callback(...arg);
+
+        const queryData = {
+          data,
+          isLoading: false,
+          error: undefined,
+          isCalled: true,
+          isError: false,
+          isSuccess: true,
+        };
+
+        setQuery(queryData);
+        return queryData;
+      } catch (error) {
+        const commonQuery = {
+          isLoading: false,
+          isCalled: true,
+          isError: true,
+          isSuccess: false,
+        };
+
+        if (error instanceof ApiError) {
+          // eslint-disable-next-line @typescript-eslint/no-shadow
+          const queryData: any = {
+            ...commonQuery,
+            statusCode: error.status,
+            error,
+          };
+
+          setQuery(queryData);
+          return queryData;
+        }
+
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        const queryData: any = {
+          ...commonQuery,
+          statusCode: undefined,
+          error: {
+            message: 'Не известная ошибка',
+            path: '',
+            statusCode: 500,
+            timestamp: String(Date.now()),
+          },
+        };
+
+        setQuery(queryData);
+        return queryData;
+      }
+    },
+    [callback],
+  );
+
+  useEffect(() => {
+    if (isLazy) {
+      return;
+    }
+
+    fetch();
+  }, [id]);
+
+  return useMemo(() => ({ ...query, fetch }), [fetch, query]);
+}
+
+export { useQuery };
