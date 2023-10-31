830593e5dba109135e24dacdd74a07903953c1f3
Sergei Zheludkov <58815292+sergei-zheludkov@users.noreply.github.com>
2023-10-31 07:14:43 +0300

    CB-0059 | shared: add function for conversion date

---
+++ packages/shared/src/date/get-day-time-to-display.ts
@@ -0,0 +1,9 @@
+const getTimeToDisplay = (time: number) => {
+  const absTime = Math.abs(time);
+  const hours = String(Math.floor(absTime / 60));
+  const minutes = String(absTime % 60).padStart(2, '0');
+
+  return `${hours}:${minutes}`;
+};
+
+export { getTimeToDisplay };
--- packages/shared/src/date/index.ts
+++ packages/shared/src/date/index.ts
@@ -1,1 +1,2 @@
 export { getTodayStart } from './get-today-start';
+export { getTimeToDisplay } from './get-day-time-to-display';
